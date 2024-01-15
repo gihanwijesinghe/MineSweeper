c2b722e430eb0a174405f5f4b5d5fe5209feb4c8
Gihan <gihanwijesinghe12@gmail.com>
2024-01-15 06:25:04 +0530

    Added mocking framework

--- MineSweeperTests/GameGeneratorTests.cs
+++ MineSweeperTests/GameGeneratorTests.cs
@@ -1,5 +1,7 @@
 ﻿using MineSweeper;
+using MineSweeper.Commands;
 using MineSweeper.Models;
+using Moq;
 
 namespace MineSweeperTests
 {
@@ -24,6 +26,33 @@ namespace MineSweeperTests
 
             // Assert
             Assert.That(actual, Is.EqualTo(GameResult.Lose));
+        }
+
+        [TestCase(0, 0, GameResult.Lose)]
+        [TestCase(0, 1, GameResult.Lose)]
+        [TestCase(0, 2, GameResult.Lose)]
+        public void ShouldLoseRunTest(int x, int y, GameResult expected)
+        {
+            // Arrange
+            var randomGenerator = new MockRandomGenerator();
+            var mineFieldGenerator = new MineFieldGenerator(randomGenerator);
+
+            Mock<IUserCommand> mockUserCommand = new Mock<IUserCommand>();
+            mockUserCommand.Setup(x => x.PromptSquareSelection(It.IsAny<int>())).Returns((x, y));
+            mockUserCommand.Setup(x => x.PromptPlayAgain(It.IsAny<bool>(), It.IsAny<bool>())).Returns(false);
+            mockUserCommand.Setup(x => x.PromptNumberOfMines(It.IsAny<int>())).Returns(3);
+            mockUserCommand.Setup(x => x.PromptGridSize()).Returns(3);
+
+            var mockInputOutput = new MockInputOutput();
+            var gameGenerator = new GameGenerator(mockUserCommand.Object, mockInputOutput, mineFieldGenerator);
+
+            gameGenerator.Run();
+
+            // Act
+            var actual = gameGenerator.MineGame.CurrentResult;
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
